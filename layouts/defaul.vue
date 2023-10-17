2da7037954efb1c6d54fd0dd9af368e4b7113a0b
titapuspitas <141605952+titapuspitas@users.noreply.github.com>
2023-10-17 09:25:40 +0700

    Add files via upload

---
+++ layouts/defaul.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+       <h1>catatan pengunjung perpustakaan.</h1>
+       <hr>
+
+       <slot/>
+    </div>
+</template>
---
+++ page/tambah.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1>isi pengunjung</h1>
+        <form>
+            <input type="text" placeholder="nama ..." required> <br>
+            <input type="text" placeholder="anggota ..." required> <br>
+            <textarea cols="30"  rows="10" placeholder="keperluan ..."> </textarea> <br>
+            <button type="sumbit">kirim</button>
+        </form>
+    </div>
+</template>
